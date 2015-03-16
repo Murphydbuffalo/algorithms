e1febb5e3298eec41d1fbdf8e1d778feef61a53c
Dan Murphy <murphydbuffal@gmail.com>
2015-03-15 21:45:16 -0400

    Manually hoisted variable declarations.

--- dynamic_connectivity/quick_find.js
+++ dynamic_connectivity/quick_find.js
@@ -1,9 +1,11 @@
+var implementation, i, index, bValue;
+
 module.exports = function(n){
     'use strict';
 
-    var implementation = {ids: new Array(n)};
+    implementation = {ids: new Array(n)};
 
-    for(var i = 0; i < n; i++){
+    for(i = 0; i < n; i++){
         implementation.ids[i] = i;
     }
 
@@ -17,9 +19,9 @@ module.exports = function(n){
             return false; 
         }
 
-        var i, bValue = this.ids[b];
+        bValue = this.ids[b];
 
-        for(i = 0; i < this.ids.length; i++){
+        for(index = 0; i < this.ids.length; i++){
             if(this.ids[i] === bValue){
                 this.ids[i] = this.ids[a];
             }
