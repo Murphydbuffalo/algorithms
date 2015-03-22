1046462df42d58a65f4f19f823a3e08411d82b19
Dan Murphy <murphydbuffal@gmail.com>
2015-03-21 22:13:48 -0400

    Added an implementation of weighted quick union with path compression (setting the id of elements to the id of their parents any time the tree is traversed. An extra constant cost with big time savings in finding the root of an element).

---
+++ dynamic_connectivity/weighted_quick_union_with_path_compression.js
@@ -0,0 +1,46 @@
+var implementation, i, size, rootA, rootB;
+
+module.exports = function(n){
+  'use strict';
+
+  implementation = { ids: new Array(n), size: [] };
+
+  for(i = 0; i < n; i++){ 
+    implementation.ids[i] = i; 
+    implementation.size[i] = 1;
+  }
+
+  implementation.findRoot = function(index){
+    while(index !== this.ids[index]){ 
+      /* 
+        Path compression: rather than simply returning the id of a node's root,
+        as you traverse up the tree, set each node's id to its parent's id. This
+        means the next call to findRoot() will arrive  at the root of the tree 
+        more quickly.
+      */
+      this.ids[index] = this.ids[this.ids[index]];
+      index = this.ids[index]; 
+    }
+
+    return index;
+  };
+
+  implementation.connected = function(a, b){
+    return this.findRoot(a) === this.findRoot(b);
+  };
+
+  implementation.union = function(a, b){
+    rootA = this.findRoot(a);
+    rootB = this.findRoot(b);
+
+    if(this.size[rootA] >= this.size[rootB]){
+      this.size[rootA] += this.size[rootB];
+      return this.ids[rootB] = rootA; 
+    } else {
+      this.size[rootB] += this.size[rootA];
+      return this.ids[rootA] = rootB; 
+    }
+  };
+
+  return implementation;
+};
