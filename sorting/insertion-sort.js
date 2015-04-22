f5e124c90429ec83a76a96fc8d076a67c8c1f6b1
Dan Murphy <murphydbuffal@gmail.com>
2015-04-21 22:41:54 -0400

    Added implementation of insertion sort.

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,27 @@
+module.exports = (array) => {
+  'use strict';
+
+  const swap = (a, b) => {
+    const aValue = array[a];
+    array[a] = array[b];
+    array[b] = aValue;
+  };
+
+  const length = array.length;
+
+  let index = 0;
+
+  for(index; index < length; index++) {
+    let i = 0;
+
+    for(i; i <= index; i++) {
+      if(array[index] < array[index - i]) {
+        swap(index - i, index);
+        index = index - i;
+        i = 0;
+      }
+    }
+  }
+
+  return array;
+};
